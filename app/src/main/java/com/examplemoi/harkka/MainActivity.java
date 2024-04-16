591b66b9c0f17ad6f5875632cbbb8c966d67ebdf
Alvar <alvar.dahlskog@gmail.com>
2024-04-16 15:38:43 +0300

    Fragments

--- app/src/main/java/com/examplemoi/harkka/MainActivity.java
+++ app/src/main/java/com/examplemoi/harkka/MainActivity.java
@@ -1,6 +1,8 @@
 package com.examplemoi.harkka;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,10 +18,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.btnSearch), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+    public void switchToFragments(View view) {
+        Intent intent = new Intent(this, MunicipalityActivity.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/examplemoi/harkka/MunicipalityActivity.java
+++ app/src/main/java/com/examplemoi/harkka/MunicipalityActivity.java
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MunicipalityActivity extends AppCompatActivity {
 
@@ -15,10 +18,40 @@ public class MunicipalityActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_municipality);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.btnSearch), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        ViewPager2 viewArea = findViewById(R.id.viewArea);
+        TabPagerAdapter tabPagerAdapter = new TabPagerAdapter(this);
+        viewArea.setAdapter(tabPagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewArea.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        viewArea.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tabLayout.getTabAt(position).select();
+            }
+        });
     }
+
 }
