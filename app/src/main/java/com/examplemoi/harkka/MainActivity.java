ef7cb0eacca200258ce658bf4027360b6269e123
Alvar <alvar.dahlskog@gmail.com>
2024-04-18 17:10:15 +0300

    Fragments

--- app/src/main/java/com/examplemoi/harkka/MainActivity.java
+++ app/src/main/java/com/examplemoi/harkka/MainActivity.java
@@ -3,6 +3,7 @@ package com.examplemoi.harkka;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,20 +12,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    // terveiset rekolta
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.btnSearch), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
-    public void switchToFragments(View view) {
+    public void switchToMunicipality(View view) {
         Intent intent = new Intent(this, MunicipalityActivity.class);
         startActivity(intent);
     }
